936ec3c8d7df5610abd74afc10f1f2bc577630d3
IgnorantAI <131192343+IgnorantAI@users.noreply.github.com>
2025-05-06 20:26:26 +0200

    Update script.js
    Now it should work... i was young and foolish...

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 // ScoreTracking
 let playerWin = 0;
 let computerWin = 0;
-let rounds 
+let rounds;
 
 function decide() {
-    rounds = parseInt(prompt("How many rounds would you like to play? "));
-    if (isNaN(rounds) || rounds <0) {
-        alert("Very funny... now write an actual number, not in letters...")
-              decide();
-    }
-    if (rounds === 0) {
-        alert('Awesome, you just lost! Now fuck off');
-              }
+  rounds = parseInt(prompt(`How many rounds would you like to play? `));
+  if (isNaN(rounds) || rounds < 0) {
+    alert(`Very funny... now write an actual number, not in letters...`);
+    decide();
+  }
+  if (rounds === 0) {
+    alert(`Awesome, you just lost! Now fuck off`);
+  }
 }
 
 decide();
@@ -22,113 +22,109 @@ let computerChoice;
 let playerChoice;
 
 function start(rounds) {
-    for (i=0;i<rounds;i++) {
-        getPlayerChoice();
-        getComputerChoice();
-        playRound();
-    }
-    
-    if (playerWin === computerWin) {
-        return console.log("Seems like it's a tie!");
-    }
-    
-    if (playerWin < computerWin) {
-        return console.log("You lost the game, better luck next time");
-    }
-    else {
-        return console.log("You got lucky");
-    }
+  for (i = 0; i < rounds; i++) {
+    getPlayerChoice();
+    getComputerChoice();
+    playRound();
+  }
+
+  if (playerWin === computerWin) {
+    return console.log(`Seems like it's a tie!`);
+  }
+
+  if (playerWin < computerWin) {
+    return console.log(`You lost the game, better luck next time`);
+  } else {
+    return console.log(`You got lucky`);
+  }
 }
 
 // Getting Player's choice
-function getPlayerChoice() { 
-    playerChoice = prompt("Pick your hand: ")
-    console.log(choice);
-    if ((playerChoice.toLowerCase()) === "rock") {
-        console.log("You picked Rock")
-    }
-    if ((playerChoice.toLowerCase()) === "paper") {
-        console.log("You picked Paper")
-    }
-    if ((playerChoice.toLowerCase()) === "scissors") {
-        console.log("You picked Scissors")
-    }
-    else {
-        console.log("Are you sure you spelled that right? It's Rock, Paper or Scissors");
-        return getPlayerChoice();
-    }
+function getPlayerChoice() {
+  playerChoice = prompt(`Pick your hand: `);
+  playerChoice = playerChoice.toLowerCase();
+  console.log(playerChoice);
+  if (playerChoice.toLowerCase() === `rock`) {
+    console.log(`You picked Rock`);
+  } else if (playerChoice.toLowerCase() === `paper`) {
+    console.log(`You picked Paper`);
+  } else if (playerChoice.toLowerCase() === `scissors`) {
+    console.log(`You picked Scissors`);
+  } else {
+    console.log(
+      `Are you sure you spelled that right? It's Rock, Paper or Scissors`
+    );
+    return getPlayerChoice();
+  }
 }
 
 // getting pc's choice
 function getComputerChoice() {
-    let n = getRandomInt(3)
-    if (n<1) {
-        computerChoice = 'rock';
-        console.log("The pc chose Rock");
-    }
-    if (n>1) {
-        computerChoice = 'scissors';
-        console.log("The pc chose Scissors");
-    }
-    else {
-        computerChoice = 'paper';
-        console.log("The pc chose Paper");
-    }
+  let n = getRandomInt(3);
+  if (n === 0) {
+    computerChoice = `rock`;
+    console.log(`The pc chose Rock`);
+  }
+  if (n === 1) {
+    computerChoice = `scissors`;
+    console.log(`The pc chose Scissors`);
+  }
+  if (n === 2) {
+    computerChoice = `paper`;
+    console.log(`The pc chose Paper`);
+  }
 }
 
 // Round's flow and results
 function playRound() {
+  if (playerChoice === computerChoice) {
+    return console.log(`It's a tie!`);
+  }
 
-    if (playerChoice === computerChoice) {
-        return toTie();
-    }
+  // if they picked Rock
+  if (playerChoice === `rock` && computerChoice === `paper`) {
+    console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);
+    return toLose();
+  }
+  if (playerChoice === `rock` && computerChoice === `scissors`) {
+    console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);
+    return toWin();
+  }
+  // if they picked Paper
+  if (playerChoice === `paper` && computerChoice === `scissors`) {
+    console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);
+    return toLose();
+  }
+  if (playerChoice === `paper` && computerChoice === `rock`) {
+    console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);
+    return toWin();
+  }
 
-    // if they picked Rock
-    if ((playerChoice === 'rock') && (computerChoice === paper)) {
-        console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);
-        return toLose();
-    }
-    if ((playerChoice === 'rock') && (computerChoice === scissors)) {
-        console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);
-        return toWin();
-    }
-    // if they picked Paper
-    if ((playerChoice === 'paper') && (computerChoice === scissors)) {
-        console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);return toLose();
-    }
-    if ((playerChoice === 'paper') && (computerChoice === rock)) {
-        console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);return toWin();
-    }
-    
-    // if they picked Scissors
-    if ((playerChoice === 'scissors') && (computerChoice === rock)) {
-        console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);return toLose();
-    }
-    if ((playerChoice === 'scissors') && (computerChoice === paper)) {
-        console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);return toWin();
-    }
-    
+  // if they picked Scissors
+  if (playerChoice === `scissors` && computerChoice === `rock`) {
+    console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);
+    return toLose();
+  }
+  if (playerChoice === `scissors` && computerChoice === `paper`) {
+    console.log(`you chose ${playerChoice} and the PC chose ${computerChoice}`);
+    return toWin();
+  }
 }
 
 function toLose() {
-    computerWin += 1;
-    return console.log("You lost");
+  computerWin += 1;
+  return console.log(`You lost`);
 }
 
 function toWin() {
-    playerWin += 1;
-    return console.log("You won!");
+  playerWin += 1;
+  return console.log(`You won!`);
 }
-
-function toTie() {
-    return console.log("It's a tie!");
-}
-
 
 // random value math
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
+  return Math.floor(Math.random() * max);
+}
+
 // Chose how many rounds to play
 start(rounds);
